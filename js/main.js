c7f136bd470bd3d4e702d63519419c59bfa06903
oli_home <ocundale@gmail.com>
2013-07-31 23:41:49 +0100

    more touch ups

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 
 $(function() {
 
-	function fadeContent() {
+$(window).load(function(){
+  function fadeContent() {
 	    $('#container').fadeIn(600).css({'top': '106px'}, 500);
 	    $('.nav').animate({ marginTop: '20px'}, 500); //.css({backgroundColor: 'rgba(000,000,000,0.9)'});
 	}
 	//Listeners
 	$('img.bg').fadeIn(2000);
 	setTimeout(fadeContent, 1500);
-	$('.galleryls').mouseover(function() {
 	
-	}).mouseout(function() {
-	
-	});
 	$("#galleryls").jFlip(800,533,{background:"black",cornersTop:false}).bind("flip.jflip",function(event,index,total){
   		$("#l1").html("Image "+(index+1)+" of "+total);
 	});
+});
+
+	
 });  
 
 
