91ef8632dd0782627d43f30dcbd181eb96114049
oli_home <ocundale@gmail.com>
2013-09-05 19:12:18 +0100

    latest changes

--- js/main.js
+++ js/main.js
@@ -9,19 +9,19 @@ $(function() {
 		if (typeof page !== 'undefined') {			
 			switch (page) {
 				case "container":
-					$('#contact_page, #prints_page').fadeOut(function() {
+					$('#contact_page, #prints_page').fadeOut(1000, function() {
 				    	$('#container').fadeIn(100);
 				    });
 				    break;
 				
 				case "prints_page":
-					$('#contact_page, #container').fadeOut(function() {
+					$('#contact_page, #container').fadeOut(1000, function() {
 				    	$('#prints_page').fadeIn(100);
 				    });
 				    break;
 
 				case "contact_page":
-					$('#container, #prints_page').fadeOut(function() {
+					$('#container, #prints_page').fadeOut(1000, function() {
 				    	$('#contact_page').fadeIn(100);
 				    });
 				    break;
@@ -30,11 +30,17 @@ $(function() {
 	}
 
 	//Listeners
-	$('.go_link').bind('click', switchPage);
+	$(document).ready(function() {
+	$('.go_link').on('click', switchPage);
+	$('#contact_page').click(function(e){
+    $('.copyright').fadeOut(1000,function(){
+	      alert("fadeOut() method is finished!");
+	    });
+	  });
 	console.log('go ahead.....inspect me');
 	setTimeout(function(){console.log('son of a....');}, 6000)
 
-//});
+});
 	
 });  
 
